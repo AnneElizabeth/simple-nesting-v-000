be705dd772e2938c942fe5445a5b21be669b2e8d
Anne Edmondson <edmondson.anne@gmail.com>
2018-08-24 03:42:01 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -16,11 +16,10 @@ def hopper
         }
      }
 
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
  		{
@@ -37,6 +36,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+   
+   programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,15 +56,11 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -79,6 +76,14 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     
+  # programmer_hash[:yukihiro_matsumoto]
+  # << [:known_for]
+  # programmer_hash[:yukihiro_matsumoto][:known_for] << "Ruby"
+  # programmer_hash[:yukihiro_matsumoto] << [:languages]
+  # programmer_hash[:yukihiro_matsumoto][:languages]<< ["Ruby","LISP", "C"]
+
+  # adding_matz
 
     
 end
